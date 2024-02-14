4f678b521067447d2b842845c21a9260256a38d7
AnnaZhukovskaja <kulnis.anna@gmail.com>
2024-02-14 10:29:00 +0300

    TMS-8 change Base url

--- src/test/java/wrappers/DropDown.java
+++ src/test/java/wrappers/DropDown.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 @Log4j2
-public class DropDownTestCase {
+public class DropDown {
     String label;
-    public String DROPDOWN_TEST_CASE_XPATH ="//*[contains(@class,'chzn-container-active')]//li[contains(text(),'%s')]";
+    public String DROPDOWN_XPATH ="//*[contains(@class,'chzn-container-active')]//li[contains(text(),'%s')]";
 
     public String CLICK_DROPDOWN_XPATH = "//*[contains(text(),'%s')]/../*/*[contains(@class,'chzn-single')]";
 
-    public DropDownTestCase(String label) {
+    public DropDown(String label) {
         this.label = label;
     }
-    public void selectForTestCase(String selectName) {
+    public void select(String selectName) {
         log.info("Selecting '{}' inside dropdown '{}'",selectName,label);
         $(By.xpath(String.format(CLICK_DROPDOWN_XPATH,label))).click();
-        $(By.xpath(String.format(DROPDOWN_TEST_CASE_XPATH,selectName))).click();
+        $(By.xpath(String.format(DROPDOWN_XPATH,selectName))).click();
     }
 }
